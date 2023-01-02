dd58dc52d1d1f187ced06d3791424c5c0967e87c
soyeonjwa <81522548+soyeonjwa@users.noreply.github.com>
2023-01-02 21:24:57 +0900

    [Silver V] Title: 숫자 카드, Time: 612 ms, Memory: 7060 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -1,27 +1,33 @@
-
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+int card_num[500000]={0,};
+
+int binary_search(int size, int n){
+    int start = 0; int end = size-1;
+    while(start<=end){
+        int m = (start+end)/2;
+        if(card_num[m]==n) return 1;
+        
+        if(card_num[m]>n) end = m-1;
+        else start = m+1;
+    }
+    return 0;
+}
 
 int main() {
     int n; int x;
     cin >> n;
     
-    vector<int> card(10000001);
-    for(int i=0;i<10000001;i++) card[i]=0;
     
     for(int i=0;i<n;i++){
         cin >> x;
-        if(x<0){
-            if(card[abs(x)]==1) card[abs(x)]=2;
-            else card[abs(x)]=-1;
-        }
-        else{
-            if(card[x]==-1) card[x]=2;
-            else card[x]=1;
-        }
+        card_num[i]=x;
     }
+    sort(card_num,card_num+n);
     
     int m;
     cin >> m;
@@ -29,14 +35,7 @@ int main() {
     vector<int> ans;
     for(int i=0;i<m;i++){
         cin >> x;
-        if(x<0){
-            if(card[abs(x)]==2||card[abs(x)]==-1) ans.push_back(1);
-            else ans.push_back(0);
-        }
-        else{
-            if(card[x]==2||card[x]==1) ans.push_back(1);
-            else ans.push_back(0);
-        }
+        ans.push_back(binary_search(n, x));
     }
     
     for(int i=0;i<ans.size();i++) cout << ans[i] <<" ";
