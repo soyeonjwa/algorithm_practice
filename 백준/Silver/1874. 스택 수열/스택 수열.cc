122c7d12b0062c8b8a391af60e9bb5b92165cb50
soyeonjwa <81522548+soyeonjwa@users.noreply.github.com>
2023-04-08 01:40:25 +0900

    [Silver II] Title: 스택 수열, Time: 56 ms, Memory: 2688 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -9,38 +9,6 @@
 
 using namespace std;
 
-bool verify(int arr[], int n){
-    int visited[n];
-    
-    for(int i=0;i<n;i++) visited[i]=0;
-    
-    for(int i=0;i<n;i++){
-        if(visited[i]==1) continue;
-        
-        visited[i]=1;
-        
-        int big = arr[i];
-        int sub = arr[i];
-        
-        for(int j=i+1;j<n;j++){
-            
-            if(arr[j] < big){
-                if(arr[j]<sub){
-                    
-                    visited[j] = 1;
-                    sub = arr[j];
-                }
-                else{
-                    return 0;
-                }
-                
-            }
-        }
-    }
-    
-    
-    return 1;
-}
 
 int main() {
     int n;
@@ -55,48 +23,43 @@ int main() {
     }
     
    
-    if(verify(arr, n)){
-        vector<char> ans;
-        stack<int> s;
+    vector<char> ans;
+    stack<int> s;
         
-        int arr_num =1;
-        int t = 0;
+    int arr_num =1;
+    int t = 0;
         
         
-        for(int i=0;i<n;i++){
-            if(t<arr[i]){
-                for(int j=arr_num;j<arr[i]+1;j++){
-                    s.push(arr_num);
-                    arr_num++;
+    for(int i=0;i<n;i++){
+        if(t<arr[i]){
+            for(int j=arr_num;j<arr[i]+1;j++){
+                s.push(arr_num);
+                arr_num++;
                     
                     
-                    cout << "+" <<'\n';
-                }
-                
-                s.pop();
-                cout << "-" << '\n';
-                
-                if(s.size()!=0) t = s.top();
+                ans.push_back('+');
             }
-            else if(t == arr[i]){
-                s.pop();
-                cout << "-" << '\n';
                 
-                if(s.size()!=0) t = s.top();
-            }
-            else{
-                cout << "NO" << '\n';
-               // return 0;
-            }
-            
+            s.pop();
+            ans.push_back('-');
+                
+            if(s.size()!=0) t = s.top();
         }
-        
+        else if(t == arr[i]){
+            s.pop();
+            ans.push_back('-');
+                
+            if(s.size()!=0) t = s.top();
+        }
+        else{
+            cout << "NO";
+            return 0;
+        }
+            
     }
-    else{
-        cout << "NO";
-        return 0;
+  
+    for(int i=0;i<ans.size();i++){
+        cout << ans[i] << '\n';
     }
-    
-    
     return 0;
 }
