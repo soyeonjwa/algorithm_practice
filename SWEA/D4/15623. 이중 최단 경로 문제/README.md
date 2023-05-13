9d378f2ad95346a0c8dd734a2f335f463c3d3de6
soyonii <81522548+soyeonjwa@users.noreply.github.com>
2023-05-14 03:42:15 +0900

    [D4] Title: 이중 최단 경로 문제, Time: 18 ms, Memory: 13,680 KB -BaekjoonHub

---
+++ SWEA/D4/15623. 이중 최단 경로 문제/README.md
@@ -0,0 +1,11 @@
+# [D4] 이중 최단 경로 문제 - 15623 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYOCL0GaCF4DFATW) 
+
+### 성능 요약
+
+메모리: 13,680 KB, 시간: 18 ms, 코드길이: 4,405 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/15623. 이중 최단 경로 문제/이중 최단 경로 문제.cpp
@@ -0,0 +1,131 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+#include <climits>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int INF = INT_MAX;
+typedef struct info{
+    int xSum;
+    int ySum;
+    int res;
+}info;
+
+int dijistra(int start, int n, vector<pair<int, pair<int,int> > > graph[]){
+    vector<info> m(n+1);
+    for(int i=1;i<=n;i++){
+        m[i].xSum = INF;
+        m[i].ySum = INF;
+        m[i].res = INF;
+    }
+    
+    m[start].xSum = 0; m[start].ySum = 0; m[start].res = 0;
+    
+    priority_queue<pair<int,int> > pq; //first: -distance, second: nodenum
+    pq.push({0,start});
+    
+    while(!pq.empty()){
+        int curNode = pq.top().second;
+        int curSum = -pq.top().first;
+        
+        pq.pop();
+        
+        for(int i=0;i<graph[curNode].size();i++){
+            int nextSum = (m[curNode].xSum+graph[curNode][i].second.first)*(m[curNode].ySum+graph[curNode][i].second.second);
+            int nextNode = graph[curNode][i].first;
+            
+            if(nextSum < m[nextNode].res){
+                m[nextNode].res = nextSum;
+                m[nextNode].xSum = m[curNode].xSum+graph[curNode][i].second.first;
+                m[nextNode].ySum = m[curNode].ySum+graph[curNode][i].second.second;
+                
+                pq.push({-nextSum, nextNode});
+            }
+        }
+    }
+    
+    
+    return m[2].res;
+}
+                
+
+int main(int argc, char** argv)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.tie(NULL);
+    
+	int test_case;
+	int T;
+	/*
+	   아래의 freopen 함수는 input.txt 를 read only 형식으로 연 후,
+	   앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+	   //여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+	   freopen 함수를 이용하면 이후 cin 을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+	   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 함수를 사용하셔도 좋습니다.
+	   freopen 함수를 사용하기 위해서는 #include <cstdio>, 혹은 #include <stdio.h> 가 필요합니다.
+	   단, 채점을 위해 코드를 제출하실 때에는 반드시 freopen 함수를 지우거나 주석 처리 하셔야 합니다.
+	*/
+	//freopen("input.txt", "r", stdin);
+	cin>>T;
+	/*
+	   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+	*/
+	for(test_case = 1; test_case <= T; ++test_case)
+	{
+
+		int n,m;
+        cin >> n >> m;
+        
+        vector<pair<int,pair<int,int> > > graph[n+1];
+        for(int i=0;i<m;i++){
+            int a,b,x,y;
+            cin >> a>> b>> x>> y;
+            
+            graph[a].push_back({b,{x,y}});
+            graph[b].push_back({a,{x,y}});
+        }
+        
+       	int ans = dijistra(1,n,graph);
+            
+		cout <<"#" <<test_case <<" ";
+        if(ans==INF) cout << -1 << '\n';
+        else cout << ans <<'\n';
+
+	}
+	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+}
